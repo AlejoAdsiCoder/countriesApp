d78dd2ecd96c66ffcfde31370890da28a8477108
Alejandro <leho.patino@gmail.com>
2022-02-15 11:12:37 -0500

    configs eslint prettier and sass

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import Nav from './menu/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
